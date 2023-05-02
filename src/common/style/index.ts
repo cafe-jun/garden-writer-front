5053d502e01f463b9f2e59781778315740807d67
givvemee <givvemeee@gmail.com>
2023-05-03 00:01:33 +0900

    [feat] added stitches to style, header base

---
+++ src/common/style/index.ts
@@ -0,0 +1,10 @@
+import { styled } from '@stitches/react';
+
+export const Wrap1440 = styled('div', {
+  width: '1440px',
+  margin: '0 auto',
+});
+
+export const Flex = styled('div', {
+  display: 'flex',
+});
---
+++ src/components/Header/style.ts
@@ -0,0 +1,27 @@
+import { Flex } from '@/common/style';
+import { styled } from 'lib/stitches.config';
+
+export const HeaderContainer = styled('div', {
+  border: '1px solid #D9D9D9',
+  height: '70px',
+  background: '$white',
+});
+
+export const HeaderSizing = styled(Flex, {
+  width: '100%',
+  height: '70px',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+});
+
+export const MenuWrap = styled('div', {
+  cursor: 'pointer',
+  display: 'flex',
+});
+
+export const Menu = styled('p', {
+  color: '$primary',
+  fontSize: '$f16',
+  marginRight: '50px',
+  fontWeight: '500',
+});
