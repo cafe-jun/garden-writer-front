02686e055f31978207b604324ae6e370a01f0350
HyunHo Lee <dlgusgh2001@naver.com>
2023-05-07 14:00:04 +0900

    [chore] eslint, prettier 탄탄하게 잡기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-import Header from '@/components/Header';
 import { globalStyle } from 'lib/stitches.config';
+// import { Inter } from 'next/font/google';
+import Head from 'next/head';
 
-const inter = Inter({ subsets: ['latin'] });
+import Header from '@/components/Header';
+
+// const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   globalStyle();
