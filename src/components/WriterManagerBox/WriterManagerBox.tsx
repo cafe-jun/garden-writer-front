e322c3442606e36a8630a78255d63bbef880734c
doctorpresso <jins.brrr@gmail.com>
2024-03-27 22:02:46 +0900

    [feat] writer sequence box

--- src/components/WriterManagerBox/WriterManagerBox.tsx
+++ src/components/WriterManagerBox/WriterManagerBox.tsx
@@ -16,53 +16,87 @@ import {
   sortableKeyboardCoordinates,
   verticalListSortingStrategy,
 } from '@dnd-kit/sortable';
-import { ReactElement, useId, useState } from 'react';
+import { ReactElement, useId, useMemo, useState } from 'react';
+
+import { config } from '@/config/config';
+import { novelJoinWriteList } from '@/fetch/get';
+import { useQueryWrap } from '@/hooks/reactQeuryWrapper';
+import { useUrlDatas } from '@/hooks/useUrlDatas';
 
 import DndItem from '../DndItem/DndItem';
 import { WriterManagerBoxProps } from './type';
 import st from './WriterManagerBox.module.scss';
 
-export default function WriterManagerBox({
-  data,
-  handleDragEnd,
-}: WriterManagerBoxProps): ReactElement {
+export default function WriterManagerBox({ handleDragEnd }: WriterManagerBoxProps): ReactElement {
   const sensors = useSensors(
     useSensor(PointerSensor),
     useSensor(KeyboardSensor, {
       coordinateGetter: sortableKeyboardCoordinates,
     })
   );
+  const roomId = useUrlDatas();
   const [modifyMode, setModifyMode] = useState<boolean>(false);
   const id: string = useId();
   const [activeId, setActiveId] = useState<UniqueIdentifier | null>(null);
+  const { data: writerList, isSuccess } = useQueryWrap({
+    queryKey: [config.apiUrl.novelJoinWriterList, roomId],
+    queryFn: () => novelJoinWriteList(roomId),
+  });
+  const data = useMemo(() => writerList?.data ?? [], [writerList]);
+
+  const modifyCancel = (): void => {
+    setModifyMode(false);
+  };
+  const doModify = () => {
+    setModifyMode(true);
+  };
+  const handleDragStart = (event: DragStartEvent) => {
+    if (!event) {
+      setActiveId(null);
+    }
+    setActiveId(event.active.id);
+  };
+  const onDragEnd = (e: DragEndEvent) => {
+    setActiveId(null);
+    handleDragEnd(e);
+  };
   return (
     <DndContext
       sensors={sensors}
       id={id}
       collisionDetection={closestCenter}
       onDragStart={handleDragStart}
-      onDragEnd={(e: DragEndEvent) => {
-        setActiveId(null);
-        handleDragEnd(e);
-      }}
+      onDragEnd={onDragEnd}
     >
       <div className={st.column}>
         <div className={st.box}>
-          <div className={st.box_title}>참여작가(3/5)</div>
+          <div className={st.box_title}>참여작가({data.length}/5)</div>
           <SortableContext
             disabled={!modifyMode}
-            items={data}
+            items={data ?? []}
             strategy={verticalListSortingStrategy}
           >
-            {data.map((item: string) => (
+            {data.map(item => (
               // 마우스가 드래그를 하면 마우스를 따라오는 것이 아닌 드래그 아이템이 놓아질 위치에 그려지는 element
-              <DndItem overlayMode={false} disabled={!modifyMode} id={item} key={item} />
+              <DndItem
+                {...item}
+                overlayMode={false}
+                disabled={!modifyMode}
+                id={item.id}
+                key={item.id}
+              />
             ))}
           </SortableContext>
 
           {/* 마우스가 드래그를 시작하면 마우스를 따라오는 오버레이 element */}
           <DragOverlay dropAnimation={null}>
-            {activeId ? <DndItem overlayMode={false} disabled={!modifyMode} id={activeId} /> : null}
+            {activeId && data.length > 0 ? (
+              <DndItem
+                overlayMode={false}
+                disabled={!modifyMode}
+                {...data.find(item => item.id === activeId)}
+              />
+            ) : null}
           </DragOverlay>
 
           {!modifyMode ? (
@@ -87,17 +121,4 @@ export default function WriterManagerBox({
       </div>
     </DndContext>
   );
-
-  function modifyCancel(): void {
-    setModifyMode(false);
-  }
-  function doModify() {
-    setModifyMode(true);
-  }
-  function handleDragStart(event: DragStartEvent) {
-    if (!event) {
-      setActiveId(null);
-    }
-    setActiveId(event.active.id);
-  }
 }
--- src/components/WriterManagerBox/type.ts
+++ src/components/WriterManagerBox/type.ts
@@ -1,7 +1,5 @@
 import { DragEndEvent } from '@dnd-kit/core';
 
 export interface WriterManagerBoxProps {
-  data: string[];
-
   handleDragEnd(event: DragEndEvent): void;
 }
