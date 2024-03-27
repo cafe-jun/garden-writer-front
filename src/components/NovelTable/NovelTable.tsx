a9d7aa06c9ecd1cb2ea831c2337589e2bd17b2ba
doctorpresso <jins.brrr@gmail.com>
2024-03-27 22:04:03 +0900

    [feat] 참여중인 공방 리스트

--- src/components/NovelTable/NovelTable.tsx
+++ src/components/NovelTable/NovelTable.tsx
@@ -1,39 +1,48 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { ReactElement } from 'react';
 
 import styles from './NovelTable.module.scss';
 import { NovelTableProps } from './type';
 
-export const NovelTable = ({ tableData }: NovelTableProps) => (
-  <table className={styles.table}>
-    <thead>
-      <tr>
-        <th>No</th>
-        <th>카테고리</th>
-        <th>제목</th>
-        <th>개설일</th>
-        <th>완결일</th>
-        <th>작가 구분</th>
-        <th>정원</th>
-        <th>현 작성자</th>
-        <th>소설공방 상태</th>
-      </tr>
-    </thead>
-    <tbody>
-      {tableData.map((item, index) => (
-        <tr key={item.id}>
-          <td>{index + 1}</td>
-          <td>{item.category}</td>
-          <td>{item.title}</td>
-          <td>{item.createdAt}</td>
-          <td>{item.completionAt}</td>
-          <td>{item.writerStatus}</td>
-          <td>
-            {item.currentAttendCnt}/{item.currentWriterCnt}
-          </td>
-          <td>{item.id}</td>
-          <td>{item.status}</td>
+export const NovelTable = ({ tableData }: NovelTableProps): ReactElement => {
+  const route = useRouter();
+  return (
+    <table className={styles.table}>
+      <thead>
+        <tr>
+          <th>No</th>
+          <th>카테고리</th>
+          <th>제목</th>
+          <th>개설일</th>
+          <th>완결일</th>
+          <th>작가 구분</th>
+          <th>정원</th>
+          <th>현 작성자</th>
+          <th>소설공방 상태</th>
         </tr>
-      ))}
-    </tbody>
-  </table>
-);
+      </thead>
+      <tbody>
+        {tableData.map((item, index) => (
+          <tr
+            key={item.id}
+            onClick={() => {
+              route.push(`/write/detail?room=${item.id}`);
+            }}
+          >
+            <td>{index + 1}</td>
+            <td>{item.category}</td>
+            <td>{item.title}</td>
+            <td>{item.createdAt}</td>
+            <td>{item.completionAt}</td>
+            <td>{item.writerStatus}</td>
+            <td>
+              {item.currentAttendCnt}/{item.currentWriterCnt}
+            </td>
+            <td>{item.id}</td>
+            <td>{item.status}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
