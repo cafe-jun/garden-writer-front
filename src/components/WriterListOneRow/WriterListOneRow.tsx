e5d93840b596b50df9c870447e29ae2dee21bbf2
doctorpresso <jins.brrr@gmail.com>
2024-03-27 21:59:26 +0900

    [feat] 소설공방 작가 관리

--- src/components/WriterListOneRow/WriterListOneRow.tsx
+++ src/components/WriterListOneRow/WriterListOneRow.tsx
@@ -1,26 +1,61 @@
 import { ReactElement } from 'react';
 
-import { WriterListOneRowProps } from './type';
+import { config } from '@/config/config';
+import { updateWriterState } from '@/fetch/fetch';
+import { GetWriterListAdmin } from '@/fetch/types';
+import { useMutationWrap } from '@/hooks/reactQeuryWrapper';
+
 import st from './WriterListOneRow.module.scss';
 
-export default function WriterListOneRow({ index_ }: WriterListOneRowProps): ReactElement {
+export default function WriterListOneRow({
+  category,
+  createdAt,
+  exitAt,
+  id,
+  nickname,
+  notifiedAt,
+  status,
+  userId,
+}: GetWriterListAdmin): ReactElement {
+  const { mutate: writerStateUpdate } = useMutationWrap({
+    mutationKey: [config.apiUrl.updateWriterState(id)],
+    mutationFn: updateWriterState,
+  });
+  const join = () => {
+    writerStateUpdate({ userId: id, status: 'attending' });
+  };
+  const reject = () => {
+    writerStateUpdate({ userId: id, status: 'attendingReject' });
+  };
+  const kick = () => {
+    writerStateUpdate({ userId: id, status: 'exit' });
+  };
   return (
     <div className={st.writerRow}>
-      <p className={st.main_writerlist_Number}>1</p>
-      <p className={st.main_writerlist_nickName}>어렵다</p>
-      <p className={st.main_writerlist_textAmount}>2343</p>
-      <p className={st.main_writerlist_dateForParticipation}>12.23.23</p>
-      <p className={st.main_writerlist_approvalStatus}>12.23.32/12.23.23</p>
-      <p className={st.main_writerlist_exitDate}>12.45.6</p>
-      <p className={st.main_writerlist_participationStatus}>아직</p>
+      <p className={st.main_writerlist_Number}>{id}</p>
+      <p className={st.main_writerlist_nickName}>{nickname}</p>
+      <p className={st.main_writerlist_textAmount}>??</p>
+      <p className={st.main_writerlist_dateForParticipation}>??</p>
+      <p className={st.main_writerlist_approvalStatus}>??/??</p>
+      <p className={st.main_writerlist_exitDate}>{exitAt}</p>
+      <p className={st.main_writerlist_participationStatus}>{status}</p>
 
       <div className={st.row_btns}>
-        <button className={st.resolve} type="button">
-          승인
-        </button>
-        <button className={st.reject} type="button">
-          퇴장
-        </button>
+        {category === 'attendee' &&
+          (status === 'attending' ? (
+            <button className={st.reject} onClick={kick} type="button">
+              퇴장
+            </button>
+          ) : (
+            <>
+              <button className={st.resolve} onClick={join} type="button">
+                승인
+              </button>
+              <button className={st.reject} onClick={reject} type="button">
+                반려
+              </button>
+            </>
+          ))}
       </div>
     </div>
   );
