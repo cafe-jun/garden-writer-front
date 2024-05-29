a8a1a4c97140f001cda5625be6ad777add55b84f
doctorpresso <jins.brrr@gmail.com>
2024-05-29 19:19:31 +0900

    [feat] 작가모집글 상세페이지 수정

--- src/components/RecruitmentTable/RecruitmentTable.tsx
+++ src/components/RecruitmentTable/RecruitmentTable.tsx
@@ -1,5 +1,7 @@
+import Skeleton from '@mui/material/Skeleton';
 import Image from 'next/image';
-import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import React from 'react';
 
 import { GetWriterWantedList } from '@/fetch/types';
 import LikeIcon from '@/images/novel-like-icon.svg';
@@ -18,57 +20,65 @@ const Card = ({
   roomTitle,
   roomType,
   viewCount,
-}: GetWriterWantedList) => (
-  <button className={st.card} type="button">
-    <div>
-      <div className={st.status}>모집중</div>
-      <p>개설일 {roomCreatedAt}</p>
-    </div>
-
-    <p className={st.title}>{roomTitle}</p>
-
-    <div>
-      <p className={st.titleAndText}>
-        제목<span>{boardTitle}</span>
-        작가 인원
-        <span>{currentWriterCnt}/5</span>
-      </p>
-    </div>
-
-    <div className={st.otherInfo}>
+  roomId,
+}: GetWriterWantedList) => {
+  const router = useRouter();
+  return (
+    <button
+      className={st.card}
+      type="button"
+      onClick={() => {
+        router.push(`/recruitment/detail/${roomId}`);
+      }}
+    >
       <div>
-        <Image src={UserIcon} alt="유저 썸네일" />
-        <p>용진</p>
+        <div className={st.status}>모집중</div>
+        <p>개설일 {roomCreatedAt}</p>
       </div>
 
+      <p className={st.title}>{roomTitle}</p>
+
       <div>
-        <Image src={ViewIcon} alt="읽은 횟수" />
-        <p>{viewCount}</p>
-        <Image src={LikeIcon} alt="좋아요 수" />
-        <p>{likeCount}</p>
+        <p className={st.titleAndText}>
+          제목<span>{boardTitle}</span>
+          작가 인원
+          <span>{currentWriterCnt}/5</span>
+        </p>
       </div>
-    </div>
-  </button>
-);
+
+      <div className={st.otherInfo}>
+        <div>
+          <Image src={UserIcon} alt="유저 썸네일" />
+          <p>용진</p>
+        </div>
+
+        <div>
+          <Image src={ViewIcon} alt="읽은 횟수" />
+          <p>{viewCount}</p>
+          <Image src={LikeIcon} alt="좋아요 수" />
+          <p>{likeCount}</p>
+        </div>
+      </div>
+    </button>
+  );
+};
 export const RecruitmentTable = ({ data, isLoading }: Props) => {
   const handleOnClick = (): void => {
     //
   };
-  console.log(isLoading);
-  useEffect(() => {
-    console.log(isLoading);
-  }, [isLoading]);
   return (
     <div className={st.content}>
-      {/* {new Array(6).fill(0).map(()=><Skeleton key={item} variant="rectangular" width="33.75px" height="11.125px" />)} */}
-      {data.map(item => (
-        // if (isLoading) {
-        //   return (
-
-        //   );
-        // }
-        <Card {...item} key={item.roomId} />
-      ))}
+      {isLoading &&
+        new Array(6)
+          .fill(0)
+          .map((item, index) => (
+            <Skeleton
+              key={index.toString() + 1}
+              animation="wave"
+              sx={{ width: '33.75rem', height: '11.125rem', transform: 'none' }}
+            />
+          ))}
+      {!isLoading && data.map(item => <Card {...item} key={item.roomId} />)}
     </div>
   );
 };
