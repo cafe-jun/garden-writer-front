81dfed2180627d70415a29a03a4709257f7c951d
doctorpresso <jins.brrr@gmail.com>
2024-05-29 19:25:05 +0900

    [fix] cookie check

--- util/fetchWrapper.ts
+++ util/fetchWrapper.ts
@@ -7,19 +7,20 @@ interface CallApi {
   method: apiMethod;
 }
 export default async function callApi<T>({ url, body, method }: CallApi): Promise<T> {
-  const token = localStorage.getItem('access');
+  const token = localStorage.getItem(config.storageKey);
   const init: RequestInit = {
+    credentials: 'include',
     method,
     headers: {
       'Content-Type': 'application/json',
-      Authorization: token ? `Bearer ${token}` : '',
+      // Authorization: token ? `Bearer ${token}` : '',
     },
     body: body ? JSON.stringify(body) : undefined,
   };
   const res = await fetch(config.apiLink + url, init);
   // 에러면 에러 반환
   if (!res.ok) {
-    throw new Error(res.status.toString());
+    throw res.status;
   }
   // 200이 아닌 성공은 boolean 반환
   if (res.status !== 200) {
