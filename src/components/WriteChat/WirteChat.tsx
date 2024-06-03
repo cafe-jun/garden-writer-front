85cedea5175e139b179a6b778df6c2a579f61fb7
doctorpresso <jins.brrr@gmail.com>
2024-06-03 21:50:01 +0900

    [feat] novel chat update

--- src/components/WriteChat/WirteChat.tsx
+++ src/components/WriteChat/WirteChat.tsx
@@ -1,25 +1,31 @@
 import Image from 'next/image';
 import { ReactElement } from 'react';
 
+import { GetOneNovelText } from '@/fetch/types';
 import lockIcon from '@/images/lock.svg';
 import unLockIcon from '@/images/unlock.svg';
 
 import st from './WriteChat.module.scss';
 
-export default function WriteChat(): ReactElement {
-  const str =
-    '으으아아아아아아아아아아아아아아아아아아아아 사람 살려 라고 그는 외쳤다. 왜 줄바꿈이 안되지. 왜!!!~~\n그는 멍하니 모니터를 보고 있다';
+export default function WriteChat({
+  chapterId,
+  content,
+  createdAt,
+  id,
+  status,
+  updatedAt,
+}: GetOneNovelText): ReactElement {
   return (
     <div className={st.chat}>
       <div className={st.chat_bar}>
         <p className={st.chat_nick}>닉네임</p>
-        <p className={st.chat_status}>[상태]</p>
-        <p className={st.chat_date}>23.23.23(월) 23:23</p>
+        <p className={st.chat_status}>{status}</p>
+        <p className={st.chat_date}>{updatedAt}</p>
         <button type="button">
           <Image src={true ? lockIcon : unLockIcon} alt="자물쇠 아이콘" />
         </button>
       </div>
-      <p className={st.chat_content}>{str}</p>
+      <p className={st.chat_content}>{content}</p>
     </div>
   );
 }
