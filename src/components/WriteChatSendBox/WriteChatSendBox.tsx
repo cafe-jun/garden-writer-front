859eb3f13a2fe3bb57152e83aed19592c8fef5a5
doctorpresso <jins.brrr@gmail.com>
2024-06-03 21:50:43 +0900

    [feat] shift & enter

--- src/components/WriteChatSendBox/WriteChatSendBox.tsx
+++ src/components/WriteChatSendBox/WriteChatSendBox.tsx
@@ -1,15 +1,41 @@
 import Image from 'next/image';
-import { ReactElement } from 'react';
+import { KeyboardEvent, ReactElement, useState } from 'react';
 
+import { config } from '@/config/config';
+import { newNovelText } from '@/fetch/post';
+import { useMutationWrap } from '@/hooks/reactQeuryWrapper';
 import chatSendPlane from '@/images/chat-send-plane.svg';
 
 import st from './WriteChatSendBox.module.scss';
 
-export default function WriteChatSendBox(): ReactElement {
+interface WriteChatSendBoxProps {
+  lastNovelNo: number;
+}
+export default function WriteChatSendBox({ lastNovelNo }: WriteChatSendBoxProps): ReactElement {
+  const { mutate: sendNewChat } = useMutationWrap({
+    mutationKey: [config.apiUrl.newNovelText],
+    mutationFn: newNovelText,
+  });
+  const [text, setText] = useState<string>('');
+  const sendText = () => {
+    sendNewChat({ content: text, chapterId: lastNovelNo });
+    setText('');
+  };
+  const handleKeyDown = (event: KeyboardEvent<HTMLTextAreaElement>) => {
+    if (event.key === 'Enter' && !event.shiftKey) {
+      event.preventDefault();
+      sendText();
+      // Enter key alone pressed, you can add custom behavior here if needed.
+    }
+  };
   return (
     <div className={st.sendBox}>
-      <textarea />
-      <button type="button">
+      <textarea
+        value={text}
+        onKeyDown={handleKeyDown}
+        onChange={event => setText(event.target.value)}
+      />
+      <button type="button" onClick={() => sendText()}>
         <Image src={chatSendPlane} alt="작성한 소설 채팅 보내기 버튼" />
       </button>
     </div>
