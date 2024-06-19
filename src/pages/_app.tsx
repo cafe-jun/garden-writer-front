b2df03f695d0482761ae6c6e8e23418fcd109d20
doctorpresso <jins.brrr@gmail.com>
2024-06-19 13:37:42 +0900

    [feat] add snackbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import '../styles/global.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
+import { SnackbarProvider } from 'notistack';
 import { useEffect, useState } from 'react';
 
 import { PageHeader } from '@/components/PageHeader/PageHeader';
@@ -11,7 +12,7 @@ const queryClient = new QueryClient();
 export default function App({ Component, pageProps }: AppProps) {
   const [isHeader, setIsHeader] = useState<boolean>(false);
   const { asPath } = useRouter();
-  const banPick = ['/', '/login', '/signUp', '/write/info'];
+  const banPick = ['/', '/login', '/signUp', '/write/info', '/user/passwd'];
   useEffect(() => {
     if (banPick.includes(asPath)) {
       setIsHeader(false);
@@ -22,10 +23,12 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <QueryClientProvider client={queryClient}>
-      {/* <RightMouseDisable /> */}
-      {/* <F12Disabled /> */}
-      {isHeader && <PageHeader />}
-      <Component {...pageProps} />
+      <SnackbarProvider anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}>
+        {/* <RightMouseDisable /> */}
+        {/* <F12Disabled /> */}
+        {isHeader && <PageHeader />}
+        <Component {...pageProps} />
+      </SnackbarProvider>
     </QueryClientProvider>
   );
 }
