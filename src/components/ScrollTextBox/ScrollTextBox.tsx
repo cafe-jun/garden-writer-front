7184db5fda19f3b86ec45a98224b211cfdc3ee8b
doctorpresso <jins.brrr@gmail.com>
2024-06-19 13:36:38 +0900

    Update ScrollTextBox.tsx

--- src/components/ScrollTextBox/ScrollTextBox.tsx
+++ src/components/ScrollTextBox/ScrollTextBox.tsx
@@ -6,14 +6,13 @@ import { ScrollTextBoxProps } from './type';
 export default function ScrollTextBox({
   style,
   title,
-  date,
   disabled,
 }: ScrollTextBoxProps): ReactElement {
   return (
     <div style={style} className={`${st.container} ${disabled ? '' : st.on}`}>
       <div className={st.container_texts}>
         <p className={st.container_title}>{title}</p>
-        <p className={st.container_date}>{date}</p>
+        {/* <p className={st.container_date}>{date}</p> */}
       </div>
       <textarea disabled={disabled} className={st.container_textarea} />
     </div>
