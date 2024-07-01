edb80a7fdba4bcb6e8ed8c1b891bd93f4d4e55e5
cafe_jun <tb25271@gmail.com>
2024-07-01 21:04:43 +0900

    [feat] cors method 재설정 6

--- util/fetchWrapper.ts
+++ util/fetchWrapper.ts
@@ -9,6 +9,7 @@ interface CallApi {
 export default async function callApi<T>({ url, body, method }: CallApi): Promise<T> {
   const token = localStorage.getItem(config.storageKey);
   const init: RequestInit = {
+    mode: 'cors',
     credentials: 'include',
     method,
     headers: {
