2b039b9e515792fb5a1711912d5ad6c4f8888cf7
cafe_jun <tb25271@gmail.com>
2024-07-02 09:06:04 +0900

    [feat] endpoint 다시수정

--- util/envConfig.ts
+++ util/envConfig.ts
@@ -1,7 +1,7 @@
 export const envApiConfig = () => {
   const envConfig = process.env.NODE_ENV;
-  // if (envConfig === 'development') {
-  // }
-  // return 'http://localhost:3001';
-  return `/api`;
+  if (envConfig === 'development') {
+    return 'http://localhost:3001';
+  }
+  return 'https://port-0-garden-of-writer-server-71t02clq3bpxzf.sel4.cloudtype.app';
 };
