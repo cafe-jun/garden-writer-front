d1649a5c763f14fcea8ffeac4bfb56aa265d9b2c
Choozii <jiyechoo@gmail.com>
2024-08-27 15:37:40 +0900

    chore: index 수정

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export * from './common';
 export * from './recruit';
 
-export * from './LoginDataInput/LoginDataInput';
 export * from './WritingModeSelector/WritingModeSelector';
 export * from './NovelTabs/NovelTabs';
 export * from './modals/WorkspaceCreationModal/WorkspaceCreationModal';
