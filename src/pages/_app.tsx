885ed661a720be0f01df41d15f8aca27f810ac1e
cafe_jun <tb25271@gmail.com>
2024-09-20 22:46:33 +0900

    [deploy] sass import 경로 수정완료

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,7 +17,9 @@ const noHeaderPages = ['/user', '/join', '/write/create'];
 
 export default function App({ Component, pageProps }: AppProps) {
   const { asPath } = useRouter();
-  const showHeader = !noHeaderPages.some(pageUrl => asPath.includes(pageUrl) || pageUrl.includes(asPath));
+  const showHeader = !noHeaderPages.some(
+    pageUrl => asPath.includes(pageUrl) || pageUrl.includes(asPath)
+  );
 
   return (
     <ThemeProvider theme={theme}>
