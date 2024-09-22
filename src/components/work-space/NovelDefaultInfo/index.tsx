02acdbdc43c34b9fd830a5b38213043d8b95ac12
Chooii <jiyechoo@gmail.com>
2024-09-22 10:57:54 +0900

    refactor: NovelDefaultInfo 수정

--- src/components/work-space/NovelDefaultInfo/index.tsx
+++ src/components/work-space/NovelDefaultInfo/index.tsx
@@ -16,15 +16,19 @@ interface NovelDefaultInfoProps {
 
 export const NovelDefaultInfo = ({ isShow = false }: NovelDefaultInfoProps) => {
   const roomId = useUrlDatas<number>('room');
-  const [modifyMode, setModifyMode] = useState(false);
+  const [editMode, setEditMode] = useState(false);
 
   const { data: novelInfo, isSuccess } = useQueryWrap({
     queryKey: [config.apiUrl.novelRoomInfo(roomId), roomId],
     queryFn: () => novelRoomInfo(roomId),
   });
 
-  const toggleModify = () => {
-    setModifyMode(p => !p);
+  const toggleEditMode = () => {
+    setEditMode(prev => !prev);
+  };
+
+  const offEditMode = () => {
+    setEditMode(false);
   };
 
   return (
@@ -44,7 +48,7 @@ export const NovelDefaultInfo = ({ isShow = false }: NovelDefaultInfoProps) => {
         <div className={st.main_textColumn}>
           {isSuccess && (
             <ScrollTextBox
-              disabled={!modifyMode}
+              disabled={!editMode}
               title="소개"
               content={novelInfo.data.subTitle}
               style={{ width: '718px', height: '138px', marginLeft: '8px' }}
@@ -53,7 +57,7 @@ export const NovelDefaultInfo = ({ isShow = false }: NovelDefaultInfoProps) => {
           {!isSuccess && <Skel sx={{ width: 718, height: 138, marginLeft: 8 }} />}
 
           {isSuccess && (
-            <div className={`${st.main_tagBox} ${modifyMode ? st.mdf : ''}`}>
+            <div className={`${st.main_tagBox} ${editMode ? st.mdf : ''}`}>
               <div className={st.main_tag_flexWrap}>
                 <div className={st.main_tagBox_tag}>#{novelInfo?.data.category.name}</div>
               </div>
@@ -67,7 +71,7 @@ export const NovelDefaultInfo = ({ isShow = false }: NovelDefaultInfoProps) => {
       {isSuccess && (
         <ScrollTextBox
           content={novelInfo.data.character}
-          disabled={!modifyMode}
+          disabled={!editMode}
           title="등장인물"
           style={{ width: '996px', height: '186px', marginTop: '8px' }}
         />
@@ -78,29 +82,30 @@ export const NovelDefaultInfo = ({ isShow = false }: NovelDefaultInfoProps) => {
       {isSuccess && (
         <ScrollTextBox
           content={novelInfo.data.summary}
-          disabled={!modifyMode}
+          disabled={!editMode}
           title="줄거리"
           style={{ width: '996px', height: '186px', marginTop: '8px' }}
         />
       )}
       {!isSuccess && <Skel sx={{ width: '996px', height: '186px', marginTop: '8px' }} />}
-      {!modifyMode ? (
-        <button
-          type="button"
-          className={`white-btn ${st.main_infoModifyBtn}`}
-          onClick={toggleModify}
-        >
-          기본정보 수정
-        </button>
-      ) : (
+
+      {editMode ? (
         <div className={st.infoChangeBtn}>
-          <button onClick={toggleModify} className="blue-btn" type="button">
+          <button onClick={offEditMode} className="blue-btn" type="button">
             수정완료
           </button>
-          <button className="white-btn" type="button" onClick={() => setModifyMode(false)}>
+          <button className="white-btn" type="button" onClick={offEditMode}>
             취소
           </button>
         </div>
+      ) : (
+        <button
+          type="button"
+          className={`white-btn ${st.main_infoModifyBtn}`}
+          onClick={toggleEditMode}
+        >
+          기본정보 수정
+        </button>
       )}
     </div>
   );
